c5c358c9a7fb2d8196c5d8ffbcacccaf3f885db3
Filatov Evgen <evgen.filatov@gmail.com>
2015-08-23 09:00:27 +0300

    threeole

--- public/src/Figure.js
+++ public/src/Figure.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import classnames from 'classnames'
-import {EIGHT, SIXTEEN, SIXTEEN3} from './constans'
+import {EIGHT, SIXTEEN, SIXTEEN3, THREEOLE} from './constans'
 
 module.exports = React.createClass({
 	displayName: 'Figure',
 	render() {
-		return <div className="note-group">
-			{this.props.figure.map((note, index) => {
+		var {figure} = this.props;
+		var classes = classnames('note-group', {
+			triple: figure[0] === THREEOLE
+		});
+		return <div className={classes}>
+			{figure.map((note, index) => {
 				var classes = classnames({
 					note: true,
 					eight: note === EIGHT,
 					sixteen: note === SIXTEEN,
 					sixteen3: note === SIXTEEN3,
+					threeole: note === THREEOLE,
 					current: this.props.currentNote === index ||
 						this.props.active
 				});
